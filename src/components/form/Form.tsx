54d623dca23c26d6679eb7cbfb09ce432c230b76
MrBiedny <MrBiedny@gmail.com>
2024-05-16 20:14:49 +0200

    Initial commit

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import DatePicker from "react-datepicker";
-
 import "react-datepicker/dist/react-datepicker.css";
 
 import { useCities } from "../../contexts/CitiesContext.js";
